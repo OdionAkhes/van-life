e0f34342f9075b3f503640dfb0d337be474601be
Odion Akhes <akhetuamhenodion@gmail.com>
2023-03-25 10:12:44 +0100

    tried to set up mirage js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import About from './About';
-import Home from './Home';
+import About from './pages/About';
+import Home from './pages/Home';
+import VansList from './pages/VansList';
 import './App.css';
-import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import "./server";
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +12,7 @@ function App() {
         <Link to="/" className='site-logo'>#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
 
         </nav>
       </header>
@@ -16,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<VansList />} />
       </Routes>
     </BrowserRouter>
   );
